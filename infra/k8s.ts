9374397562e4aaaa05d52b0ce73ef90c4974b90e
Jason (Jay) Smith <jaysmith@google.com>
2023-03-06 14:20:09 -0800

    cleanup pulumi files

--- infra/k8s.ts
+++ infra/k8s.ts
@@ -1,49 +1,42 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
-import * as gcp from "@pulumi/gcp";
+import * as gcloud from "@pulumi/google-native";
 import * as k8s from "@pulumi/kubernetes";
 import * as pulumi from "@pulumi/pulumi";
-import { masterVersion, nodeCount, nodeMachineType, password, username } from "./config";
+import * as config from "./config";
 
-// Create the GKE cluster and export it.
-export const k8sCluster = new gcp.container.Cluster("gke-cluster", {
-    // We can't create a cluster with no node pool defined, but we want to only use
-    // separately managed node pools. So we create the smallest possible default
-    // node pool and immediately delete it.
-    initialNodeCount: 1,
-    removeDefaultNodePool: true,
-
-    minMasterVersion: masterVersion,
-});
-
-const nodePool = new gcp.container.NodePool(`primary-node-pool`, {
-    cluster: k8sCluster.name,
-    initialNodeCount: nodeCount,
-    location: k8sCluster.location,
-    nodeConfig: {
-        preemptible: true,
-        machineType: nodeMachineType,
-        oauthScopes: [
-            "https://www.googleapis.com/auth/compute",
-            "https://www.googleapis.com/auth/devstorage.read_only",
-            "https://www.googleapis.com/auth/logging.write",
-            "https://www.googleapis.com/auth/monitoring",
-        ],
+const cluster = new gcloud.container.v1.Cluster("cluster", {
+    location: config.region,
+    autopilot: { enabled: true },
+    project: config.projectId,
+    releaseChannel: {
+        channel: gcloud.container.v1.ReleaseChannelChannel.Regular,
     },
-    version: masterVersion,
-    management: {
-        autoRepair: true,
+    nodePools: [
+        {
+            name: "initial",
+            config: {
+                oauthScopes: [
+                    "https://www.googleapis.com/auth/devstorage.read_only",
+                    "https://www.googleapis.com/auth/logging.write",
+                    "https://www.googleapis.com/auth/monitoring",
+                    "https://www.googleapis.com/auth/service.management.readonly",
+                    "https://www.googleapis.com/auth/servicecontrol",
+                    "https://www.googleapis.com/auth/trace.append",
+                    "https://www.googleapis.com/auth/compute",
+                ],
+            },
+        },
+    ],
+    workloadIdentityConfig: {
+        workloadPool: `${config.projectId}.svc.id.goog`,
     },
-}, {
-    dependsOn: [k8sCluster],
 });
 
 // Manufacture a GKE-style Kubeconfig. Note that this is slightly "different" because of the way GKE requires
 // gcloud to be in the picture for cluster authentication (rather than using the client cert/key directly).
-export const k8sConfig = pulumi.
-    all([ k8sCluster.name, k8sCluster.endpoint, k8sCluster.masterAuth ]).
-    apply(([ name, endpoint, auth ]) => {
-        const context = `${gcp.config.project}_${gcp.config.zone}_${name}`;
+export const kubeConfig = pulumi
+    .all([cluster.name, cluster.endpoint, cluster.location, cluster.masterAuth])
+    .apply(([name, endpoint, location, auth]) => {
+        const context = `${config.projectId}_${location}_${name}`;
         return `apiVersion: v1
 clusters:
 - cluster:
@@ -71,8 +64,12 @@ users:
     });
 
 // Export a Kubernetes provider instance that uses our cluster from above.
-export const k8sProvider = new k8s.Provider("gkeK8s", {
-    kubeconfig: k8sConfig,
-}, {
-    dependsOn: [nodePool],
-});
+export const provider = new k8s.Provider(
+    "gke-k8s",
+    {
+        kubeconfig: kubeConfig,
+    },
+    {
+        dependsOn: [cluster],
+    }
+);
